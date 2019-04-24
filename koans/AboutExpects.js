9cbcff56fbcfd8ef871309765aa5964560904bc7
MrKamake <seongjooon@gmail.com>
2019-04-24 16:36:17 +0900

    complete first mission

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -10,13 +10,13 @@ describe('About Expects', function() {
   // To understand reality, we must compare our expectations against reality.
   it('should expect equality', function() {
     // TODO: FILL_ME_IN 변수에 할당된 값을 올바르게 수정해주세요.
-    var expectedValue = FILL_ME_IN;
+    var expectedValue = 2;
     var actualValue = 1 + 1;
 
     // expect, toBeTruthy 등은 어떤 주어진 값을 판별하는 용도의 Jasmine Library에서 제공하는 함수입니다.
     // [ expect ] https://jasmine.github.io/api/3.3/global.html#expect
     // [ toBeTruthy ] https://jasmine.github.io/api/3.3/matchers.html#toBeTruthy
-    expect(actualValue !== expectedValue).toBeTruthy();
+    expect(actualValue === expectedValue).toBeTruthy();
   });
 
   // Some ways of asserting equality are better than others.
